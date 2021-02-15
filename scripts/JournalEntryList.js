888b46bb34901c2388ecb0aa440dec757b774f50
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-15 10:33:57 -0600

    refactoring to no longer use journalentry.js module

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -2,18 +2,22 @@ import { getEntries, useJournalEntries } from "./JournalDataProvider.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
 // DOM reference to where all entries will be rendered
-const entryLog = document.querySelector("#entryLog")
+const contentTarget = document.querySelector("#entryLog")
 
-export const EntryListComponent = () => {
+export const EntryList = () => {
     // Use the journal entry data from the data provider component
     const entries = getEntries()
-    console.log(Array.isArray(entries))
-    for (const entry of entries) {
-        /*
-            Invoke the component that returns an
-            HTML representation of a single entry
-        */
-        let DOMString = JournalEntryComponent(entry)
-        entryLog.innerHTML += DOMString
-    }
+    .then(render(entries))
 }
+
+const render = (entriesArray) => {
+    contentTarget.innerHTML = 
+        `<h3>Entry #${entriesArray.id}</h3>
+            ${entriesArray.map(entry =>{
+                return`
+                    <section id="entry--${entry.id}" class="journalEntry">
+                        <p>- ${entry.date}</p>
+                        <p>${entry.concept}</p>
+                        <p>${entry.mood}</p>
+                        <p>${entry.entry}</p>
+                    </section>`}).join("")}
